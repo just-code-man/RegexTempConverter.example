e0896ce60e7629659f680da8d8577dab5d35e959
just-code-man <36886851+just-code-man@users.noreply.github.com>
2024-08-18 16:57:54 +0800

    Initial commit

---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using Luke.RegexTempConverter;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp = "My name is #Name#";
+
+            IRegexTemp tp = RegexTempFactory.Greate("test", new TempEngineBuilderConfig
+            {
+                Pattern = "#(\\S*)#",
+                Template = temp,
+            });
+
+            var paramBuilder = tp.CreateParamBuilder();
+
+            paramBuilder.IfExist("Name", "Luke");
+
+            var tm = paramBuilder.Build();
+
+            string result = tp.Execute(tm);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
